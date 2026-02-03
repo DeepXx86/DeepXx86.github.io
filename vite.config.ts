c29029efbd3f1013ce59345c1111511b3c2095d7
TEERANON <teeranon@TEERANONs-MacBook-Air.local>
2026-02-03 15:57:20 +0700

    fix: change base to relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DeepXx86.github.ioTeeranon-Saraman.github.io/',
+  base: './',
   plugins: [react()],
 })
